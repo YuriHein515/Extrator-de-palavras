8402a2b228ac0bc939987b87bd6418d84b384d05
YuriHein515 <yuri.hein@escola.pr.gov.br>
2025-09-05 17:06:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+const botaoMostraPalavras = document.querySelector("#botao-palavrachave");
+botaoMostraPalavras.addEventListener("click" , mostrarPalavrasChaves);
+function mostrarPalavrasChaves(){
+    alert("Fui clicado aeeeeeeeeee to feliz por que fui clicado :)!");
+}
